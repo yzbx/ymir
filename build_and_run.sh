4c5911bd55a01e7e5b18ed4d9705f2d0f50da55f
youdaoyzbx <youdaoyzbx@163.com>
2022-11-10 11:12:33 +0800

    update zzz

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,6 @@
-cd ymir && docker build -t industryessentials/ymir-backend:1.3.0 --build-arg PIP_SOURCE=https://pypi.mirrors.ustc.edu.cn/simple --build-arg SERVER_MODE='dev' -f Dockerfile.backend .
+cd ymir && docker build -t industryessentials/ymir-backend:2.0.0 --build-arg PIP_SOURCE=https://pypi.mirrors.ustc.edu.cn/simple --build-arg SERVER_MODE='dev' -f Dockerfile.backend .
 
-cd web && docker build -t industryessentials/ymir-web:1.3.0 --build-arg NPM_REGISTRY='https://registry.npm.taobao.org' .
+cd web && docker build -t industryessentials/ymir-web:2.0.0 --build-arg NPM_REGISTRY='https://registry.npm.taobao.org' .
 
 cd ../..
 docker-compose down -v && docker-compose up -d
